64d3c2c93831d71cac086c74903c5acfd55db306
houdret <houdret@users.noreply.github.com>
2022-03-14 11:29:11 +0100

    crud

---
+++ WindowsFormsContacts/WindowsFormsContacts/BusinessLogicLayer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsContacts
+{
+    public class BusinessLogicLayer
+    {
+        private DataAccessLayer _dataAccessLayer;
+
+        public BusinessLogicLayer()
+        {
+            _dataAccessLayer = new DataAccessLayer();
+        }
+        public Contact SaveContact(Contact contact)
+        {
+            if (contact.Id == 0)
+            {
+                _dataAccessLayer.InsertContact(contact);
+            }
+            else
+            {
+                _dataAccessLayer.UpdateContact(contact);
+            }
+
+            return contact;
+        }
+
+        public List<Contact> GetContacts(string search = null)
+        {
+            return _dataAccessLayer.GetContacts(search);
+        }
+
+        public void DeleteContact(int id)
+        {
+            _dataAccessLayer.DeleteContact(id);
+        }
+    }
+}
